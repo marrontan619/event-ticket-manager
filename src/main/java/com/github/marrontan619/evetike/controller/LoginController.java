a18b67a5a78604bd7ee9237e5c7f7e92506b90de
青木涼太 <marrontan619@gmail.com>
2015-07-07 17:45:10 +0900

    eventテーブルからフェッチしてトップページに表示

--- src/main/java/com/github/marrontan619/evetike/controller/LoginController.java
+++ src/main/java/com/github/marrontan619/evetike/controller/LoginController.java
@@ -1,9 +1,14 @@
 package com.github.marrontan619.evetike.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.github.marrontan619.evetike.model.Event;
+import com.github.marrontan619.evetike.repository.EventRepository;
 import com.github.marrontan619.evetike.repository.UsersRepository;
 
 @Controller
@@ -12,9 +17,15 @@ public class LoginController {
     @Autowired
     UsersRepository userRepository;
 
+    @Autowired
+    EventRepository eventRepository;
+
     @RequestMapping(value = "/top")
-    public String top() {
-        return "top";
+    public ModelAndView top(ModelAndView mav) {
+        List<Event> events = eventRepository.findAll();
+        mav.addObject("events", events);
+        mav.setViewName("top");
+        return mav;
     }
 
     @RequestMapping(value = "/loginFailure")
